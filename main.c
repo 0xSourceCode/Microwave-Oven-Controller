5001e47c1803e8db517cb743e447898741b0f923
kareem <104324839+kareem62@users.noreply.github.com>
2022-05-16 00:06:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
- 
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+#include "DIO.h"
+#include "keybad.h"
+
+
+void popcorn(void)
+{
+	char button = keypad_read('B')
+		if (button == 'A')
+		{
+			LCD_moveCursor(char 0, char 1);
+			LCD_sendString(char 'Popcorn');
+			LCD_clearScreen(void);
+			uint32_t i;
+			for (i = 1; i < 61; i++) {
+				LCD_sendString(char i)
+					LCD_clearScreen(void);
+				wait_1sec();
+
+			}
+		}
