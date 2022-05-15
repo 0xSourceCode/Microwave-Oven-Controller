25347904c7cd38745cbf16691cff0f9bc979e61a
Bahnasy2001 <105355425+Bahnasy2001@users.noreply.github.com>
2022-05-15 23:44:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
- 
+#include "stdint.h"
+#include "tm4c123gh6pm.h"
+#include "BITCTRL.h"
+#include "DIO.h"
+#include "Timer.h"
+#include "keypad.h"
+#include "Switch.h"
+#include "LED.h"
+#include "LCD.h"
+
+
+void SystemInit(void);
+
+unsigned char button_in;
+
+void popcorn()
+	{
+		 button_in = keypad_read('A');
+		if (button_in == 'A')
+			{
+				LCD_vInit();
+				 wait_secs(60);
+				LCD_clearScreen();
+		}
+	
+}
