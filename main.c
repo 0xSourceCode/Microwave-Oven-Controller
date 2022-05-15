377be71647e9194e558ce9f045670b31e7898df1
kareem <104324839+kareem62@users.noreply.github.com>
2022-05-16 00:44:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 void popcorn(void)
 {
-	char button = keypad_read('B')
+	char button = keypad_read('A')
 		if (button == 'A')
 		{
 			LCD_moveCursor(char 0, char 1);
@@ -21,4 +21,36 @@ void popcorn(void)
 
 			}
 		}
+
+	void  Beef, chicken(void)
+	{
+		char button = keypad_read('A')
+			if (button == 'B')
+			{
+				LCD_moveCursor(char 0, char 1);
+				LCD_sendString(char 'number');
+				LCD_moveCursor(char 1, char 1);
+				LCD_sendString(char 'kilos');
+				char button = keypad_read('A')
+					LCD_clearScreen(void);
+				if (button == '0')
+				{
+					LCD_moveCursor(char 0, char 1)
+						LCD_sendString(char 'Err');
+					wait_secs(uint32_t 2);
+					LCD_clearScreen(void);
+				}
+				else {
+
+					LCD_moveCursor(char 0, char 1);
+					LCD_sendString(char button);
+					wait_secs(uint32_t 2);
+					LCD_clearScreen(void);
+					char deforset_m = 0.5 * button;
+					char deforset_sec = deforset_m * 60;
+
+					for (i = 1; i < deforset_sec; i++) {
+						LCD_sendString(char i)
+							LCD_clearScreen(void);
+						wait_1sec();
 
