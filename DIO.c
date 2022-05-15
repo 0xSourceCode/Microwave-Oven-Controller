5d4c3590eb879780e717a43194c34cdccba05eab
Youssef Ashraf <youssef.0530@hotmail.com>
2022-05-15 16:42:55 +0200

    DIO.c fixed

--- DIO.c
+++ DIO.c
@@ -1,9 +1,6 @@
 #include "stdint.h"
 #include "tm4c123gh6pm.h" 
 #include "BITCTRL.h"
-#define NVIC_ST_CTRL_R (*((volatile unsigned long *)0xE000E010))
-#define NVIC_ST_RELOAD_R (*((volatile unsigned long *)0xE000E014))
-#define NVIC_ST_CURRENT_R (*((volatile unsigned long *)0xE000E018))
 
 
 //initialize port by name
