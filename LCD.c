a76ca1c749ffec7cf92d5621d1332ee72dc7df73
Youssef Ashraf <youssef.0530@hotmail.com>
2022-05-15 13:33:26 +0200

    LED.h added to LCD.c

--- LCD.c
+++ LCD.c
@@ -1,14 +1,3 @@
 #include "DIO.h"
+#include "LED.h"
 
-void LED_vInit(unsigned char portname, unsigned char pin_number){
-	
-	DIO_vSetPinDir(portname, pin_number, 1);
-	
-
-}
-
-void LED_vON(unsigned char portname, unsigned char pin_number){
-	
-	DIO_vWritePin(portname, pin_number, 1);
-	
-}
